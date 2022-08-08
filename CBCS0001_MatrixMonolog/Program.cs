62a34e1d0a6e4a188509f903b37ef48fdea3edd6
NazarovIlya <eliakohai@gmail.com>
2022-08-08 21:33:15 +0300

    matrix monolog

---
+++ CBCS0001_MatrixMonolog/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Clear();
+
+Console.WriteLine("Wake up, Neo...");
+Console.ReadLine();
+Console.WriteLine("The Matrix has you...");
+Console.ReadLine();
+Console.WriteLine("Follow the white rabbit.");
+Console.ReadLine();
+Console.WriteLine("Knock, knock, Neo.");
+Console.ReadLine();
