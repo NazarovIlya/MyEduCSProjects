909e6813cf6ba66f6a3c772878603f708ee20dab
NazarovIlya <eliakohai@gmail.com>
2022-08-23 13:47:34 +0300

    data types completed

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -8,6 +8,7 @@ namespace DataTypes
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
             string message = "\tТип: {0, 8} |\t.NET = {1, 8} |\tРазмер = {2} MIN = {3, 30}\t| MAX =  {4}";
@@ -25,9 +26,24 @@ namespace DataTypes
             Console.WriteLine(string.Format(message, "long", typeof(long), sizeof(long), long.MinValue, long.MaxValue));
             Console.WriteLine(string.Format(message, "ulong", typeof(ulong), sizeof(ulong), ulong.MinValue, ulong.MaxValue));
 
-            Console.WriteLine("\n\t\t\t\t\t Числовые (целочисленные) типы:");
+            Console.WriteLine("\n\t\t\t\t\t Числовые (с плавающей запятой) типы:");
 
-            Console.WriteLine("\n\t\t\t\t\t Числовые (целочисленные) типы:");
+            Console.WriteLine(string.Format(message, "float", typeof(float), sizeof(float), float.MinValue, float.MaxValue));
+            Console.WriteLine(string.Format(message, "double", typeof(double), sizeof(double), double.MinValue, double.MaxValue));
+
+            Console.WriteLine("\n\t\t\t\t\t Символьные типы:");
+
+            Console.WriteLine(string.Format(message, "char", typeof(char), sizeof(char), char.MinValue, char.MaxValue));
+            Console.WriteLine(string.Format(message, "string", typeof(string), "N/A", "N/A", "N/A"));
+
+            Console.WriteLine("\n\t\t\t\t\t Логический тип:");
+
+            Console.WriteLine(string.Format(message, "bool", typeof(bool), sizeof(bool), char.MinValue, "true"));
+
+            Console.WriteLine("\n\t\t\t\t\t Особые типы:");
+
+            Console.WriteLine(string.Format(message, "object", typeof(object), "N/A", "N/A", "N/A"));
+            Console.WriteLine(string.Format(message, "dynamic", "N/A", "N/A", "N/A", "N/A"));
 
             Console.ReadKey();
         }
