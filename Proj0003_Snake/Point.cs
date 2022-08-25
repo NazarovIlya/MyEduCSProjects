738bc55209097383654ffedcf3b0f020e4de9508
NazarovIlya <eliakohai@gmail.com>
2022-08-25 14:41:53 +0300

    point class initial

---
+++ Proj0003_Snake/Point.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj0003_Snake
+{
+    internal class Point
+    {
+    }
+}
