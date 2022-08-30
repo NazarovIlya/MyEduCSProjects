84ecbf7df7e9f6654dfc88250e832a94a1370458
NazarovIlya <eliakohai@gmail.com>
2022-08-31 01:07:53 +0300

    simple calculator completed

--- Proj0004_Calculator/Program.cs
+++ Proj0004_Calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,33 @@ namespace Proj0004_Calculator
     {
         static void Main(string[] args)
         {
+            Input input = new Input();
+            Expression expression = new Expression();
+            input.Read();
+            expression.First = input.First;
+            expression.Second = input.Second;
+            expression.Symbol = input.Symbol;
+            //    public void Action()
+            //    {
+            double result = 0;
+                switch (input.Symbol)
+                {
+                    case '+':
+                        result = expression.Addition();
+                        break;
+                    case '-':
+                        result = expression.Substraction();
+                        break;
+                    case '*':
+                        result = expression.Multiplication();
+                        break;
+                    case '/':
+                        result = expression.Division();
+                        break;
+                }
+        //    }
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
