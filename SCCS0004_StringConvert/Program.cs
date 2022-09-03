048e75f29b3cbef73ba61b33dae04dab43bda043
NazarovIlya <eliakohai@gmail.com>
2022-09-03 23:19:28 +0300

    convert string

---
+++ SCCS0004_StringConvert/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Globalization;
+
+Console.Clear();
+
+Console.WriteLine("Enter first integer:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second integer:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = a + b;
+Console.WriteLine($"{a} + {b} = {result}" );
+
+Console.WriteLine("Enter first integer:");
+double c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second integer:");
+double d = Convert.ToInt32(Console.ReadLine());
+
+double resultSub = c / d;
+Console.WriteLine($"{c} / {d} = {resultSub}");
+
+string str = "3,14";
+double f = Convert.ToDouble(str);
+Console.WriteLine(f);
+
+//string str = "3.14"; // separator!!!
+//double f = Convert.ToDouble(str);
+//Console.WriteLine(f);
+//NumberFormatInfo numberFormatInfo = new NumberFormatInfo() // using System.Globalization; for use
+//{
+//    NumberDecimalSeparator = ".",
+//};
+//str = "3.14";
+//f = Convert.ToDouble(str);
+//Console.WriteLine(f);
