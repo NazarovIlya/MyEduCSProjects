84984acaa216f2cd4e3d2b3447663fc0a2fa73ca
NazarovIlya <eliakohai@gmail.com>
2022-09-04 00:04:05 +0300

    try parse

---
+++ SCCS0005_StringParseTryParse/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Clear();
+string str = "4234 fgsd dbn";
+
+try
+{
+    int a = int.Parse(str);
+    Console.WriteLine("Конвертация прошла успешно!");
+}
+catch
+{
+    Console.WriteLine("Ошибка конвертации!");
+}
+// OR:
+
+bool b = int.TryParse(str);
+
+if(b)
+    Console.WriteLine("Конвертация прошла успешно!");
+Console.WriteLine("Ошибка конвертации!");
