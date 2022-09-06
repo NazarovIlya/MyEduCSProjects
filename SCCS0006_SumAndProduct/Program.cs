61445b0341fe5c2ba810e3108ff30fe2891f76ba
NazarovIlya <eliakohai@gmail.com>
2022-09-06 21:23:30 +0300

    sum and product

---
+++ SCCS0006_SumAndProduct/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Clear();
+
+Console.WriteLine();
+string[] array = Console.ReadLine().Split(' ', ',');
+
+int a = Convert.ToInt32((string)array[0]);
+int b = Convert.ToInt32((string)array[1]);
+int c = Convert.ToInt32((string)array[2]);
+
+int sum = a + b + c;
+int product = a * b * c;
+
+Console.WriteLine($"Сумма чисел {a}, {b}, {c} = {sum}");
+Console.WriteLine($"Произведение чисел {a}, {b}, {c} = {product}");
