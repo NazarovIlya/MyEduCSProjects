2bcd38565e21d7b87a27293cce1f5f6b0f6d8a3d
NazarovIlya <eliakohai@gmail.com>
2022-09-06 15:24:20 +0300

    initial

---
+++ SCCS0006_ArethmitecOperators/Input.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCCS0006_ArethmitecOperators
+{
+    internal class Input
+    {
+        Expression expression = new Expression();
+        public Input()
+        {
+        }
+
+        public void ReadFromConsole()
+        {
+            Console.WriteLine();
+            string[] array = Console.ReadLine().Split(' ', ',');
+            expression.First = Convert.ToDouble(array[0]);
+            expression.Second = Convert.ToDouble(array[1]);
+        }
+    }
+}
---
+++ SCCS0006_ArethmitecOperators/Program.cs
@@ -0,0 +1,8 @@
+﻿using SCCS0006_ArethmitecOperators;
+
+Expression expressions = new Expression();
+Input input = new Input();
+
+input.ReadFromConsole();
+expressions.Plus();
+
