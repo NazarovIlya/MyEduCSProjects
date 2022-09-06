44cb47d75a80904f84a704ca732ceca55f09c2d5
NazarovIlya <eliakohai@gmail.com>
2022-09-06 20:55:28 +0300

    completed

--- SCCS0006_ArethmitecOperators/Program.cs
+++ SCCS0006_ArethmitecOperators/Program.cs
@@ -1,8 +1,41 @@
-﻿using SCCS0006_ArethmitecOperators;
+﻿
+Console.Clear();
+double a = 0,
+       b = 0,
+       result = 0;
 
-Expression expressions = new Expression();
-Input input = new Input();
+Console.WriteLine("Введите первое число для сложения: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число для сложения: ");
+b = Convert.ToDouble(Console.ReadLine());
+result = a + b;
+Console.WriteLine(result);
 
-input.ReadFromConsole();
-expressions.Plus();
 
+Console.WriteLine("Введите первое число для вычитания");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число для вычитания: ");
+b = Convert.ToDouble(Console.ReadLine());
+result = a - b;
+Console.WriteLine(result);
+
+Console.WriteLine("Введите первое число для умножения: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число для умножения: ");
+b = Convert.ToDouble(Console.ReadLine());
+result = a * b;
+Console.WriteLine(result);
+
+Console.WriteLine("Введите первое число для деления: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число для деления: ");
+b = Convert.ToDouble(Console.ReadLine());
+result = a / b;
+Console.WriteLine(result);
+
+Console.WriteLine("Введите первое число для деления по модулю: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число для деления по модулю: ");
+b = Convert.ToDouble(Console.ReadLine());
+result = a % b;
+Console.WriteLine(result);
