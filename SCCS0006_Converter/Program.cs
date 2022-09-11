bfa727268e971ef876399335f0c1ca2f361cd492
NazarovIlya <eliakohai@gmail.com>
2022-09-11 21:19:21 +0300

    completed

---
+++ SCCS0006_Converter/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Clear();
+
+Console.WriteLine("Сколько рублей Вы хотели бы обменять на юани?");
+double rubls = Convert.ToDouble(Console.ReadLine());
+
+double CNY = rubls * 0.011448;
+
+Console.WriteLine($"На {rubls} рублей Вы сможете купить {CNY} юаней.");
+Console.Read();
