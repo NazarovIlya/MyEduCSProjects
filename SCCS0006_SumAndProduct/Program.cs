3571d9577737960095a3bc96e4a0d0a3a847bbee
NazarovIlya <eliakohai@gmail.com>
2022-09-11 21:23:38 +0300

    corrected

--- SCCS0006_SumAndProduct/Program.cs
+++ SCCS0006_SumAndProduct/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 Console.Clear();
 
-Console.WriteLine();
+Console.WriteLine("Введите три целых числа через пробел:");
 string[] array = Console.ReadLine().Split(' ', ',');
 
 int a = Convert.ToInt32((string)array[0]);
