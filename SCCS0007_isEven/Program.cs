20ed6fd10ba31a983aa8c9ba31904f169f2f30b6
NazarovIlya <eliakohai@gmail.com>
2022-09-12 20:48:29 +0300

    iseven

---
+++ SCCS0007_isEven/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число: ");
+double isEven = Convert.ToDouble(Console.ReadLine());
+
+if(isEven % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
+
+Console.ReadLine();
