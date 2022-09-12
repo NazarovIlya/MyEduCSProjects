5ee5c955c6452980fa741f9ae218e0949bec1341
NazarovIlya <eliakohai@gmail.com>
2022-09-12 19:31:50 +0300

    updated

--- CBCS0001_MatrixMonolog/Program.cs
+++ CBCS0001_MatrixMonolog/Program.cs
@@ -1,11 +1,20 @@
 ﻿
-Console.Clear();
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
 
+Console.Clear();
 Console.WriteLine("Wake up, Neo...");
 Console.ReadLine();
+
+Console.Clear();
 Console.WriteLine("The Matrix has you...");
 Console.ReadLine();
+
+Console.Clear();
 Console.WriteLine("Follow the white rabbit.");
 Console.ReadLine();
+
+Console.Clear();
 Console.WriteLine("Knock, knock, Neo.");
 Console.ReadLine();
