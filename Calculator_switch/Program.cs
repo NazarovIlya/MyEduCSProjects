c015f84ee56c64f8a38e9b072126fccecf6c4e91
NazarovIlya <eliakohai@gmail.com>
2022-09-13 20:08:17 +0300

    switch calculator

---
+++ Calculator_switch/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+
+// ConsoleKey consoleKey = Console.ReadKey().Key;
+
+double firstValue = 0;
+double secondValue = 0;
+double result = 0;
+string action = string.Empty;
+
+Console.WriteLine("Введите число 1:");
+firstValue = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число 1:");
+secondValue = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Выберите действие: '+', '-', '*', '/'");
+action = Console.ReadLine();
+
+
+
+
+switch (action)
+{
+    case "+":
+        result = firstValue + secondValue;
+        break;
+    case "-":
+        result = firstValue - secondValue;
+        break;
+    case "*":
+        result = firstValue * secondValue;
+        break;
+    case "/":
+        {
+            if (secondValue == 0)
+                result = 0;
+            result = firstValue / secondValue;
+        }
+        break;
+    default:
+        Console.WriteLine("Error! Can't do action");
+        break;
+}
+
+Console.Write($"{result}");
+Console.ReadLine();
