170059ca3f5837cdc4d6ceb9d4cead05d1c42a7a
NazarovIlya <eliakohai@gmail.com>
2022-09-13 07:47:30 +0300

    initial

--- CBCS0001_MatrixMonolog/Program.cs
+++ CBCS0001_MatrixMonolog/Program.cs
@@ -1,7 +1,14 @@
 ﻿
-Console.BackgroundColor = ConsoleColor.Black;
-Console.ForegroundColor = ConsoleColor.Green;
-Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+//Console.BackgroundColor = ConsoleColor.Black;
+//Console.ForegroundColor = ConsoleColor.Green;
+//Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+
+using System.Drawing;
+
+IntPtr hConsole = GetStdHandle(-11);
+SetConsoleDisplayMode(hConsole, 1, out COORD b1);
+Console.ReadLine();
+
 
 Console.Clear();
 Console.WriteLine("Wake up, Neo...");
