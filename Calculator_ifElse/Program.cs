186def3534e568943cf69a459a34e87b69e36e86
NazarovIlya <eliakohai@gmail.com>
2022-09-13 20:30:07 +0300

    ifelse calculator

--- Calculator_ifElse/Program.cs
+++ Calculator_ifElse/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+double firstValue = 0;
+double secondValue = 0;
+double result = 0;
+string action = string.Empty;
+
+Console.WriteLine("Введите число 1:");
+firstValue = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число 1:");
+secondValue = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Выберите действие: '+', '-', '*', '/'");
+action = Console.ReadLine();
+
+if (action == "+")
+{
+	result = firstValue + secondValue;
+}
+else if (action == "-")
+{
+	result = firstValue - secondValue;
+}
+else if (action == "*")
+{
+	result = firstValue * secondValue;
+}
+else if (action == "/")
+{
+	if (secondValue == 0)
+        result = 0;
+    result = firstValue / secondValue;
+	
+
+}
+
+Console.WriteLine(result);
+Console.ReadKey();
+Environment.Exit(0);
