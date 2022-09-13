60b5ee587072066f2db88de2c1c695227ab2cebb
NazarovIlya <eliakohai@gmail.com>
2022-09-13 20:35:16 +0300

    updated

--- Calculator_switch/Program.cs
+++ Calculator_switch/Program.cs
@@ -5,35 +5,40 @@ double secondValue = 0;
 double result = 0;
 string action = string.Empty;
 
-Console.WriteLine("Введите число 1:");
-firstValue = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите число 1:");
-secondValue = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Выберите действие: '+', '-', '*', '/'");
-action = Console.ReadLine();
-
-switch (action)
+while (true)
 {
-    case "+":
-        result = firstValue + secondValue;
-        break;
-    case "-":
-        result = firstValue - secondValue;
-        break;
-    case "*":
-        result = firstValue * secondValue;
-        break;
-    case "/":
-        {
-            if (secondValue == 0)
-                result = 0;
-            result = firstValue / secondValue;
-        }
-        break;
-    default:
-        Console.WriteLine("Error! Can't do action");
-        break;
-}
+    Console.Clear();
+    Console.WriteLine("Введите число 1:");
+    firstValue = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите число 1:");
+    secondValue = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Выберите действие: '+', '-', '*', '/'");
+    action = Console.ReadLine();
 
-Console.Write($"{result}");
-Console.ReadLine();
+    switch (action)
+    {
+        case "+":
+            result = firstValue + secondValue;
+            break;
+        case "-":
+            result = firstValue - secondValue;
+            break;
+        case "*":
+            result = firstValue * secondValue;
+            break;
+        case "/":
+            {
+                if (secondValue == 0)
+                    result = 0;
+                else
+                result = firstValue / secondValue;
+            }
+            break;
+        default:
+            Console.WriteLine("Error! Can't do action");
+            break;
+    }
+
+    Console.Write($"{result}");
+    Console.ReadLine();
+}
