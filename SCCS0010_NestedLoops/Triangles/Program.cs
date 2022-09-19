82a10b3e79e34876916dcd75efaf87885d2bbd07
NazarovIlya <eliakohai@gmail.com>
2022-09-19 23:01:47 +0300

    triangles

--- SCCS0010_NestedLoops/Triangles/Program.cs
+++ SCCS0010_NestedLoops/Triangles/Program.cs
@@ -5,18 +5,48 @@ for (int i = 0; i <= count; i++)
 {
 	for (int j = 0; j < i; j++)
 	{
-        Console.Write("#");
+        Console.Write("*");
     }
     Console.WriteLine();
 }
 
 Console.WriteLine();
 
-for (int i = count; i > 0; i--)
+for (int i = 0; i < count; i++)
 {
+    for (int j = count; j > i; j--)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i = 0; i <= count; i++)
+{
+    for (int j = count; j > i; j--)
+    {
+        Console.Write(" ");
+    }
     for (int j = 0; j < i; j++)
     {
-        Console.Write("#");
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i = 0; i <= count; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int j = count; j > i; j--)
+    {
+        Console.Write("*");
     }
     Console.WriteLine();
 }
