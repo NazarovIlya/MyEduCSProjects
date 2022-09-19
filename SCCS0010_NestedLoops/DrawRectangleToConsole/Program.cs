f62d40965fa47836e32dea6cc693492a748740bb
NazarovIlya <eliakohai@gmail.com>
2022-09-19 19:14:09 +0300

    draw a rectangle completed

--- SCCS0010_NestedLoops/DrawRectangleToConsole/Program.cs
+++ SCCS0010_NestedLoops/DrawRectangleToConsole/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Hello, User!");
+int height = 0;
+int wigth = 0;
+Console.WriteLine("Введите высоту прямоугольника: ");
+try
+{
+    height = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Ошибка ввода!");
+}
+Console.WriteLine("Введите ширину прямоугольника: ");
+try
+{
+    wigth = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Ошибка ввода!");
+}
+Console.Clear();
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < wigth; j++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+Console.ReadLine();
