be549a79d31fcbd227000e7fa82c2fab81970255
NazarovIlya <eliakohai@gmail.com>
2022-09-19 18:20:25 +0300

    initial

---
+++ SCCS0010_NestedLoops/WriteToConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.WriteLine("Hello, User!");
+int firstValue = 0;
+int secondValue = 0;
+Console.WriteLine("Введите количество итераций для первого цикла: ");
+try
+{
+    firstValue = Convert.ToInt32(Console.ReadLine());
+}
+catch 
+{
+    Console.WriteLine("Ошибка ввода!");
+}
+Console.WriteLine("Введите количество итераций для второго цикла: ");
+try
+{
+    secondValue = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+    Console.WriteLine("Ошибка ввода!");
+}
+
+for (int i = 0; i < firstValue; i++)
+{
+    Console.WriteLine("");
+    for (int j = 0; j < secondValue; j++)
+    {
+
+    }
+}
