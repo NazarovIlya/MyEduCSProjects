cfe03ab761f328be2c39c50c89d39db9d31ee693
NazarovIlya <eliakohai@gmail.com>
2022-10-27 21:11:30 +0300

    bubble sort initial

--- SORT/BubbleSort/Program.cs
+++ SORT/BubbleSort/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// BUBBLE SORT
+
+int[] BubbleSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+int[] arr = { 4, 7, 9, 1, -2, 5, 0, 3 };
+
+Console.WriteLine($"Исходный массив: \n[ {string.Join(", ", arr)} ]");
+int[] sortArr = BubbleSort(array: arr);
+Console.WriteLine($"Отсортированный массив: \n[ {string.Join(", ", sortArr)} ]");
