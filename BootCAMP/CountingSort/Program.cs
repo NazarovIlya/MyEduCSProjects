76d367db7a6f4ffdaae5e00fe1bca2bca54a5e1e
NazarovIlya <eliakohai@gmail.com>
2022-11-04 13:12:44 +0300

    standart COUNTING SORT only for digits completed

--- BootCAMP/CountingSort/Program.cs
+++ BootCAMP/CountingSort/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// COUNTING SORT
+
+// Only digits sort
+void CountingSortDigits(int[] array)
+{
+    int[] counters = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        counters[array[i]]++;       // int number = array[i];
+                                    // counters[number]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            array[index] = i;
+            index++;
+        }
+    }
+}
+
+
+
+int[] myArr = new[] { 0, 2, 3, 2, 1, 5, 9, 1, 1 };
+Console.WriteLine($"Исходный массив: [ {string.Join(", ", myArr)} ]");
+CountingSortDigits(myArr);
+Console.WriteLine($"Отсортированный массив: [ {string.Join(", ", myArr)} ]");
