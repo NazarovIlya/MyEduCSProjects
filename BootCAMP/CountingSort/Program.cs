885002967e56458a9145b79ad42abec9644d4b90
NazarovIlya <eliakohai@gmail.com>
2022-11-04 17:35:48 +0300

    counting sort for negative numbeers too

--- BootCAMP/CountingSort/Program.cs
+++ BootCAMP/CountingSort/Program.cs
@@ -27,12 +27,14 @@ int[] CountingSortDigits(int[] array)
 int[] CountingSortExtended(int[] array)
 {
     int max = array.Max();
+    int min = array.Min();
+    int offset = -min;
     int[] arraySorted = new int[array.Length];
-    int[] counters = new int[max + 1];
+    int[] counters = new int[max + offset + 1];
     for (int i = 0; i < array.Length; i++)
     {
-        counters[array[i]]++;   // int number = array[i];
-                                // counters[number]++;
+        counters[array[i] + offset]++;   // int number = array[i];
+                                         // counters[number]++;
     }
 
     int index = 0;
@@ -40,7 +42,7 @@ int[] CountingSortExtended(int[] array)
     {
         for (int j = 0; j < counters[i]; j++)
         {
-            arraySorted[index] = i;
+            arraySorted[index] = i - offset;
             index++;
         }
     }
@@ -50,7 +52,7 @@ int[] CountingSortExtended(int[] array)
 
 
 int[] myArrDigits = new[] { 0, 2, 3, 2, 1, 5, 9, 1, 1 };
-int[] myArrNumbers = new[] { 6, 31, 13, 13, 2, 7, 1, 6, 2, 1, 46, 24 };
+int[] myArrNumbers = new[] { 6, 31, 13, -13, 2, 7, 1, 6, 2, 1, 46, 24 };
 Console.WriteLine($"Исходный массив: [ {string.Join(", ", myArrDigits)} ]");
 int[] resArrDigits = CountingSortDigits(myArrDigits);
 Console.WriteLine($"Отсортированный массив: [ {string.Join(", ", resArrDigits)} ]");
