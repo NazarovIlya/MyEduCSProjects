7f14c9490d64f4d08af95d78182086593d8533b7
NazarovIlya <eliakohai@gmail.com>
2022-11-14 00:19:34 +0300

    server socket initial

--- BootCAMP/SimpleWebServer_Java/src/main/java/ru/WebServer/WebServer.java
+++ BootCAMP/SimpleWebServer_Java/src/main/java/ru/WebServer/WebServer.java
@@ -1,9 +1,17 @@
 package ru.WebServer;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class WebServer {
 
     public static void main(String[] args){
-        System.out.println("Your are on server!");
+        try(ServerSocket serverSocket = new ServerSocket(8088)) {
+            System.out.println("Your are on server!");
+            serverSocket.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
