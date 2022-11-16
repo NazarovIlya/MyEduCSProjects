5512ac310bd706cbf9e356c4f265bb0ed2046aee
NazarovIlya <eliakohai@gmail.com>
2022-11-16 18:56:26 +0300

    simple site with simple python server completed

---
+++ BootCAMP/PrimitiveSite_Python_HTML/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    with open('file.txt', 'r', encoding='utf-8') as file:
+        resultData = list()
+        for line in file.readlines():
+            resultData.append(tuple(line.split('\n')[0].split(';')))
+
+    return render_template('base.html', data=resultData)
+
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run()
